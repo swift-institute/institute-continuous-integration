57bc1e2d7a31d8cdf97fae288cd1e10e51e09d3d
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-22 02:05:58 +0200

    Pin Xcode asset application identity

--- Sources/Institute Continuous Integration Source/ContinuousIntegration.Source.Policy.Asset.Origin.swift
+++ Sources/Institute Continuous Integration Source/ContinuousIntegration.Source.Policy.Asset.Origin.swift
@@ -3,6 +3,11 @@ public import Institute_Continuous_Integration
 extension ContinuousIntegration.Source.Policy.Asset {
     public enum Origin: Sendable {
         case release(base: Swift.String)
-        case xcode(version: Swift.String, build: Swift.String, relativePath: Swift.String)
+        case xcode(
+            application: Swift.String,
+            version: Swift.String,
+            build: Swift.String,
+            relativePath: Swift.String
+        )
     }
 }
